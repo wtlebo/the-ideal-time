10dd2c411b5190dad250522e03fce1e2940b0de8
Bill Lebo <wtlebo@gmail.com>
2025-05-17 11:02:21 -0400

    logo-height

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -179,7 +179,7 @@ function App() {
   return (
     <div className="p-4 max-w-md mx-auto">
       <div className="flex justify-center mb-4">
-        <img src="/logo.png" alt="The Ideal Time" className="w-full max-h-48 object-contain" />
+        <img src="/logo.png" alt="The Ideal Time" className="w-full max-h-96 object-contain" />
       </div>
 
       <div className="flex gap-2 items-center mb-4">

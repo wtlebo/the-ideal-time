f1e7c0e3d2b9a4793904d5e402c1795411c930cb
Bill Lebo <wtlebo@gmail.com>
2025-05-21 19:17:57 -0400

    zvzxvzxv

--- client/src/utils/ga.js
+++ client/src/utils/ga.js
@@ -1,19 +1,47 @@
 // Initialize GA4
 console.log('GA: Initializing GA4');
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  console.log('GA: gtag called with arguments:', arguments);
-  dataLayer.push(arguments);
-}
-window.gtag = gtag;
 
-gtag('js', new Date());
-gtag('config', 'G-XFNFXH80SX');
-console.log('GA: GA4 configuration sent');
+// Create script element
+const script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XFNFXH80SX';
+script.async = true;
+
+// Create initialization script
+const initScript = document.createElement('script');
+initScript.innerHTML = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-XFNFXH80SX');
+`;
+
+// Add scripts to head
+console.log('GA: Adding GA scripts to head');
+document.head.appendChild(script);
+document.head.appendChild(initScript);
+
+// Wait for GA to be ready
+let gaReady = false;
+const checkGA = () => {
+  if (typeof window.gtag === 'function') {
+    gaReady = true;
+    console.log('GA: GA is ready');
+  } else {
+    setTimeout(checkGA, 100);
+  }
+};
+checkGA();
 
 // Export tracking functions
 export const trackPageView = (page) => {
   console.log('GA: Attempting to track page view', { page });
+  if (!gaReady) {
+    console.log('GA: GA not ready yet - queueing event');
+    window.dataLayer.push(['event', 'page_view', {
+      page_path: page
+    }]);
+    return;
+  }
   try {
     gtag('event', 'page_view', {
       page_path: page
@@ -26,6 +54,14 @@ export const trackPageView = (page) => {
 
 export const trackEvent = (action, params = {}) => {
   console.log('GA: Attempting to track event', { action, params });
+  if (!gaReady) {
+    console.log('GA: GA not ready yet - queueing event');
+    window.dataLayer.push(['event', action, {
+      event_category: 'User Interaction',
+      ...params
+    }]);
+    return;
+  }
   try {
     gtag('event', action, {
       event_category: 'User Interaction',

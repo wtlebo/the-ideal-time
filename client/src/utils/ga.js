67593ba3d76202aaa3719025ef842d5a8d5a15ed
Bill Lebo <wtlebo@gmail.com>
2025-05-21 19:24:15 -0400

    GA improvements

--- client/src/utils/ga.js
+++ client/src/utils/ga.js
@@ -10,9 +10,13 @@ script.async = true;
 const initScript = document.createElement('script');
 initScript.innerHTML = `
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  window.gtag = function() {
+    dataLayer.push(arguments);
+  };
   gtag('js', new Date());
-  gtag('config', 'G-XFNFXH80SX');
+  gtag('config', 'G-XFNFXH80SX', {
+    send_page_view: true
+  });
 `;
 
 // Add scripts to head
@@ -20,31 +24,13 @@ console.log('GA: Adding GA scripts to head');
 document.head.appendChild(script);
 document.head.appendChild(initScript);
 
-// Wait for GA to be ready
-let gaReady = false;
-const checkGA = () => {
-  if (typeof window.gtag === 'function') {
-    gaReady = true;
-    console.log('GA: GA is ready');
-  } else {
-    setTimeout(checkGA, 100);
-  }
-};
-checkGA();
-
 // Export tracking functions
 export const trackPageView = (page) => {
   console.log('GA: Attempting to track page view', { page });
-  if (!gaReady) {
-    console.log('GA: GA not ready yet - queueing event');
-    window.dataLayer.push(['event', 'page_view', {
-      page_path: page
-    }]);
-    return;
-  }
   try {
-    gtag('event', 'page_view', {
-      page_path: page
+    window.gtag('event', 'page_view', {
+      page_path: page,
+      send_to: 'G-XFNFXH80SX'
     });
     console.log('GA: Page view event sent');
   } catch (error) {
@@ -54,18 +40,11 @@ export const trackPageView = (page) => {
 
 export const trackEvent = (action, params = {}) => {
   console.log('GA: Attempting to track event', { action, params });
-  if (!gaReady) {
-    console.log('GA: GA not ready yet - queueing event');
-    window.dataLayer.push(['event', action, {
-      event_category: 'User Interaction',
-      ...params
-    }]);
-    return;
-  }
   try {
-    gtag('event', action, {
+    window.gtag('event', action, {
       event_category: 'User Interaction',
-      ...params
+      ...params,
+      send_to: 'G-XFNFXH80SX'
     });
     console.log('GA: Event sent:', { action, params });
   } catch (error) {

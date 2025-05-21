0dca65d1eb14d06dbc0c9f620c47dab37f6130f1
Bill Lebo <wtlebo@gmail.com>
2025-05-21 19:06:17 -0400

    Update Vite config for GA4 script handling

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,5 +15,12 @@ export default defineConfig({
   },
   build: {
     base: process.env.NODE_ENV === 'production' ? '' : '/',
-  },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ga: ['react-ga4']
+        }
+      }
+    }
+  }
 });

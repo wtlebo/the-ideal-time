ab5e0c47dd1b534d80d390f39d563c1737019329
Bill Lebo <wtlebo@gmail.com>
2025-05-21 17:43:55 -0400

    Switch to Analytics.js for GA tracking

--- client/src/config/analytics.js
+++ client/src/config/analytics.js
@@ -2,17 +2,19 @@
 export const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID || 'YOUR_MEASUREMENT_ID';
 
 export const trackPageView = (page) => {
-  if (typeof window.gtag === 'function' && GA_MEASUREMENT_ID) {
-    window.gtag('config', GA_MEASUREMENT_ID, {
-      page_path: page,
-    });
+  if (typeof window.ga === 'function') {
+    window.ga('set', 'page', page);
+    window.ga('send', 'pageview');
   }
 };
 
 export const trackEvent = (action, params = {}) => {
-  if (typeof window.gtag === 'function' && GA_MEASUREMENT_ID) {
-    window.gtag('event', action, {
-      ...params,
+  if (typeof window.ga === 'function') {
+    window.ga('send', 'event', {
+      eventCategory: 'User Interaction',
+      eventAction: action,
+      eventLabel: JSON.stringify(params),
+      transport: 'beacon'
     });
   }
 };

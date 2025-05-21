3e6c8e38a8a4670afdef0c3535a8080b16034f00
Bill Lebo <wtlebo@gmail.com>
2025-05-21 16:33:20 -0400

    activity details

--- client/src/config/activityDefaults.js
+++ client/src/config/activityDefaults.js
@@ -8,13 +8,13 @@ export const activityDefaults = {
     temperatureRange: [50, 110],
     windSpeedMin: 0,
     windSpeedMax: 40,
-    windSpeedRange: [0, 12],
+    windSpeedRange: [0, 8],
     skyCoverMin: 0,
     skyCoverMax: 100,
-    skyCoverRange: [0, 100],
+    skyCoverRange: [0, 75],
     precipChanceMin: 0,
     precipChanceMax: 100,
-    precipChanceRange: [0, 40],
+    precipChanceRange: [0, 25],
     daylightMin: 0, // 12:00 AM
     daylightMax: 1439, // 11:59 PM
     daylightRange: [360, 1200], // 6:00 AM to 8:00 PM

46798fdb914f37e5cfa448f82bdaaaa958689b29
Bill Lebo <wtlebo@gmail.com>
2025-05-21 19:16:39 -0400

    xc cx

--- client/src/utils/ga.js
+++ client/src/utils/ga.js
@@ -1,12 +1,15 @@
 // Initialize GA4
+console.log('GA: Initializing GA4');
 window.dataLayer = window.dataLayer || [];
 function gtag() {
+  console.log('GA: gtag called with arguments:', arguments);
   dataLayer.push(arguments);
 }
 window.gtag = gtag;
 
 gtag('js', new Date());
 gtag('config', 'G-XFNFXH80SX');
+console.log('GA: GA4 configuration sent');
 
 // Export tracking functions
 export const trackPageView = (page) => {
@@ -15,6 +18,7 @@ export const trackPageView = (page) => {
     gtag('event', 'page_view', {
       page_path: page
     });
+    console.log('GA: Page view event sent');
   } catch (error) {
     console.error('GA: Error tracking page view:', error);
   }
@@ -27,6 +31,7 @@ export const trackEvent = (action, params = {}) => {
       event_category: 'User Interaction',
       ...params
     });
+    console.log('GA: Event sent:', { action, params });
   } catch (error) {
     console.error('GA: Error tracking event:', error);
   }

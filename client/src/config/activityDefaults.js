7fd9aa76d36237077a64d0761c4aee757695e6db
Bill Lebo <wtlebo@gmail.com>
2025-05-22 18:37:20 -0400

    Add new activities and sort alphabetically

--- client/src/config/activityDefaults.js
+++ client/src/config/activityDefaults.js
@@ -1,4 +1,88 @@
 export const activityDefaults = {
+  carDriving: {
+    tideMin: -2,
+    tideMax: 15,
+    tideRange: [-2, 15],
+    temperatureMin: 0,
+    temperatureMax: 120,
+    temperatureRange: [20, 80],
+    windSpeedMin: 0,
+    windSpeedMax: 40,
+    windSpeedRange: [0, 15],
+    skyCoverMin: 0,
+    skyCoverMax: 100,
+    skyCoverRange: [0, 100],
+    precipChanceMin: 0,
+    precipChanceMax: 100,
+    precipChanceRange: [0, 20],
+    daylightMin: 0, // 12:00 AM
+    daylightMax: 1439, // 11:59 PM
+    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
+    requireDaylight: false
+  },
+  hiking: {
+    tideMin: -2,
+    tideMax: 15,
+    tideRange: [-2, 15],
+    temperatureMin: 0,
+    temperatureMax: 120,
+    temperatureRange: [20, 80],
+    windSpeedMin: 0,
+    windSpeedMax: 40,
+    windSpeedRange: [0, 15],
+    skyCoverMin: 0,
+    skyCoverMax: 100,
+    skyCoverRange: [0, 100],
+    precipChanceMin: 0,
+    precipChanceMax: 100,
+    precipChanceRange: [0, 20],
+    daylightMin: 0, // 12:00 AM
+    daylightMax: 1439, // 11:59 PM
+    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
+    requireDaylight: false
+  },
+  kayaking: {
+    tideMin: -2,
+    tideMax: 15,
+    tideRange: [5, 15],
+    temperatureMin: 0,
+    temperatureMax: 120,
+    temperatureRange: [60, 110],
+    windSpeedMin: 0,
+    windSpeedMax: 40,
+    windSpeedRange: [0, 8],
+    skyCoverMin: 0,
+    skyCoverMax: 100,
+    skyCoverRange: [0, 100],
+    precipChanceMin: 0,
+    precipChanceMax: 100,
+    precipChanceRange: [0, 40],
+    daylightMin: 0, // 12:00 AM
+    daylightMax: 1439, // 11:59 PM
+    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
+    requireDaylight: true
+  },
+  motorboating: {
+    tideMin: -2,
+    tideMax: 15,
+    tideRange: [6, 15],
+    temperatureMin: 0,
+    temperatureMax: 120,
+    temperatureRange: [60, 100],
+    windSpeedMin: 0,
+    windSpeedMax: 40,
+    windSpeedRange: [0, 15],
+    skyCoverMin: 0,
+    skyCoverMax: 100,
+    skyCoverRange: [0, 100],
+    precipChanceMin: 0,
+    precipChanceMax: 100,
+    precipChanceRange: [0, 30],
+    daylightMin: 0, // 12:00 AM
+    daylightMax: 1439, // 11:59 PM
+    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
+    requireDaylight: true
+  },
   paddleboarding: {
     tideMin: -2,
     tideMax: 15,
@@ -41,6 +125,27 @@ export const activityDefaults = {
     daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
     requireDaylight: false
   },
+  running: {
+    tideMin: -2,
+    tideMax: 15,
+    tideRange: [-2, 15],
+    temperatureMin: 0,
+    temperatureMax: 120,
+    temperatureRange: [20, 80],
+    windSpeedMin: 0,
+    windSpeedMax: 40,
+    windSpeedRange: [0, 15],
+    skyCoverMin: 0,
+    skyCoverMax: 100,
+    skyCoverRange: [0, 100],
+    precipChanceMin: 0,
+    precipChanceMax: 100,
+    precipChanceRange: [0, 20],
+    daylightMin: 0, // 12:00 AM
+    daylightMax: 1439, // 11:59 PM
+    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
+    requireDaylight: false
+  },
   sailing: {
     tideMin: -2,
     tideMax: 15,
@@ -62,28 +167,7 @@ export const activityDefaults = {
     daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
     requireDaylight: true
   },
-  motorboating: {
-    tideMin: -2,
-    tideMax: 15,
-    tideRange: [6, 15],
-    temperatureMin: 0,
-    temperatureMax: 120,
-    temperatureRange: [60, 100],
-    windSpeedMin: 0,
-    windSpeedMax: 40,
-    windSpeedRange: [0, 15],
-    skyCoverMin: 0,
-    skyCoverMax: 100,
-    skyCoverRange: [0, 100],
-    precipChanceMin: 0,
-    precipChanceMax: 100,
-    precipChanceRange: [0, 30],
-    daylightMin: 0, // 12:00 AM
-    daylightMax: 1439, // 11:59 PM
-    daylightRange: [360, 1200], // 6:00 AM to 8:00 PM
-    requireDaylight: true
-  },
-  running: {
+  sunbathing: {
     tideMin: -2,
     tideMax: 15,
     tideRange: [-2, 15],
